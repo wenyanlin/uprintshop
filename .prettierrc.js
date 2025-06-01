063b899937872891d572283c6871cf8059ae9f75
WenYanLin <clps10070623@gmail.com>
2025-06-01 20:21:47 +0800

    Updates

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  singleQuote: true, // 使用單引號
+  jsxSingleQuote: true, // JSX 中使用單引號
+  arrowParens: 'always', // 箭頭函數參數始終需要括號 (x) => x
+};
+
+export default config;
